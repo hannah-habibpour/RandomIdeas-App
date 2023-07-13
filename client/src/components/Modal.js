ddcea6ad022b51194511cdde35d90c59fe2cccc2
Hannah-Habibpour <hana.habibpor@gmail.com>
2023-07-13 11:22:35 -0400

    Created IdeaForm Component

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -7,7 +7,8 @@ class Modal {
 
     addEventListeners() {
         this._modalBtn.addEventListener('click', this.open.bind(this));
-        window.addEventListener('click',this.outsideClick.bind(this));
+        window.addEventListener('click', this.outsideClick.bind(this));
+        document.addEventListener('closemodal', () => this.close());
     }
 
     open() {
