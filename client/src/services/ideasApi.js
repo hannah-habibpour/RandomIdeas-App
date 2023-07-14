7472dca1751aaeef898972c73c4a82fe316485a1
Hannah-Habibpour <hana.habibpor@gmail.com>
2023-07-13 21:37:11 -0400

    Created Idea via Form

--- client/src/services/ideasApi.js
+++ client/src/services/ideasApi.js
@@ -8,6 +8,10 @@ class IdeasApi {
     getIdeas() {
         return axios.get(this._apiUrl);
     }
+
+    createIdea(data) {
+        return axios.post(this._apiUrl, data);
+    }
 }
 
 export default new IdeasApi(); // instead of bring it in to the component, we can just call it directly from the component
